77b4fcee94b1cdb39d9a0035de454df36daac67d
Tomas Romero <canotto90@gmail.com>
2012-12-22 02:21:10 -0300

    Partidos con mas sets ganados por derecha que por izquierda

--- grails-app/domain/intertigre/domain/Partido.groovy
+++ grails-app/domain/intertigre/domain/Partido.groovy
@@ -15,10 +15,26 @@ abstract class Partido {
 	
 	static constraints = {
 		fecha display:false
-		tercerSet nullable: true, validator: { val, obj -> if(!(val.gamesGanador > val.gamesPerdedor)){
+		tercerSet nullable: true, validator: { Sett set, Partido partido -> 
+															if(partido.tieneMasSetsGanadosPorDerechaQuePorIzquierda()) {
+																return 'El partido tiene mal los datos de los sets.' +
+																'Los games del ganador deben ir del lado izquierdo de cada set'
+															}
+															if(!(set.gamesGanador > set.gamesPerdedor)){
 														   		return 'El ultimo set debe tener un numero mayor ' + 
 																   'de games al principio que final (por ej. 6-3 y no 3-6)'
 															}
 											 }
+	}
+	
+	private boolean tieneMasSetsGanadosPorDerechaQuePorIzquierda(){
+		def ps = [primerSet.gamesGanador, primerSet.gamesPerdedor]
+		def ss = [segundoSet.gamesGanador, segundoSet.gamesPerdedor]
+		def ts = [tercerSet.gamesGanador, tercerSet.gamesPerdedor]
+		def aux = 0
+		ps[0] < ps[1] ? aux-- : aux++
+		ss[0] < ss[1] ? aux-- : aux++
+		(ts[0] != null) ? ((ts[0] < ts[1]) ? aux-- : aux++) : null
+		return aux < 1
 	}
 }
--- test/integration/intertigre/domain/PartidoSpec.groovy
+++ test/integration/intertigre/domain/PartidoSpec.groovy
@@ -2,6 +2,10 @@ package intertigre.domain
 
 import static intertigre.util.DomainFactoryService.createFecha
 
+import java.lang.invoke.MethodHandleImpl.BindCaller.T
+
+import spock.lang.IgnoreRest;
+
 class PartidoSpec extends BaseIntegrationSpec{
 
 	static Equipo equipoL = Equipo.build()
@@ -19,12 +23,44 @@ class PartidoSpec extends BaseIntegrationSpec{
 			partido.errors.allErrors.toString().contains('El ultimo set debe tener un numero mayor de ' + 
 															'games al principio que final (por ej. 6-3 y no 3-6)')
 		where: 'el partido es un single o un doble'
-			singleODoble << [Single.buildWithoutSave(primerSet: Sett.build(gamesGanador: 7, gamesPerdedor: 5), 
-									segundoSet: Sett.build(gamesGanador: 6, gamesPerdedor: 4), 
-									tercerSet: Sett.build(gamesGanador: 3, gamesPerdedor: 6), fecha: fecha, equipoGanador: equipoL),
-							 Doble.buildWithoutSave(primerSet: Sett.build(gamesGanador: 7, gamesPerdedor: 5), 
-									segundoSet: Sett.build(gamesGanador: 6, gamesPerdedor: 4), 
-									tercerSet: Sett.build(gamesGanador: 3, gamesPerdedor: 6), fecha: fecha, equipoGanador: equipoL)]
+			singleODoble << [Single.buildWithoutSave(crearPartido('7-5', '6-4', '3-6')),
+							 Doble.buildWithoutSave(crearPartido('7-5', '6-4', '3-6'))]
+	}
+	
+	def 'un partido debe tener mas sets que ganados por la izquierda que por la derecha (6-3 y no 3-6)'() {
+		given: 'un partido que tiene mas sets ganados por la derecha que por la izquierda'
+			def partido = singleODoble
+		when: 'guardo el partido'
+			partido.save()
+		then: 'no se deberia guardar'
+			Partido.findAll().size() == 0
+		and: 'deberia mostrar un mensaje de error acorde'
+			partido.errors.allErrors.toString().contains('El partido tiene mal los datos de los sets.' +
+																'Los games del ganador deben ir del lado izquierdo de cada set')
+		where: 'el partido es un single o un doble'
+			singleODoble << [
+				Single.buildWithoutSave(crearPartido('7-5', '4-6', '3-6')),
+							 Single.buildWithoutSave(crearPartido('5-7', '4-6', '3-6')),
+							 Single.buildWithoutSave(crearPartido('5-7', '4-6')),
+							 Single.buildWithoutSave(crearPartido('5-7', '4-6', '6-3')),
+							 Single.buildWithoutSave(crearPartido('5-7', '6-4', '3-6')),
+							 Doble.buildWithoutSave(crearPartido('7-5', '4-6', '3-6')),
+							 Doble.buildWithoutSave(crearPartido('5-7', '4-6', '3-6')),
+							 Doble.buildWithoutSave(crearPartido('5-7', '4-6')),
+							 Doble.buildWithoutSave(crearPartido('5-7', '4-6', '6-3')),
+							 Doble.buildWithoutSave(crearPartido('5-7', '6-4', '3-6'))
+							 ]
 	}
 
+	private Map crearPartido(primerSet, segundoSet, tercerSet = null){
+		def ps = primerSet.tokenize('-').toArray()
+		def ss = segundoSet.tokenize('-').toArray()
+		def ts = tercerSet != null ? tercerSet.tokenize('-').toArray() : ['null', 'null']
+		return [primerSet: [gamesGanador: ps[0], gamesPerdedor: ps[1]],
+				  segundoSet: [gamesGanador: ss[0], gamesPerdedor: ss[1]],
+				  tercerSet: [gamesGanador: ts[0], gamesPerdedor: ts[1]],
+				  equipoGanador: equipoL,
+				  fecha: fecha]
+	}
+	
 }

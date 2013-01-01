48472da22bb226d3c76e35e317a8fb7fccec97b2
Tomas Romero <canotto90@gmail.com>
2013-01-01 16:26:23 -0300

    Refactor PartidosSpec y reportes muestran datos de prueba

--- test/integration/intertigre/domain/PartidoSpec.groovy
+++ test/integration/intertigre/domain/PartidoSpec.groovy
@@ -1,6 +1,9 @@
 package intertigre.domain
 
 import static intertigre.util.DomainFactoryService.createFecha
+
+import java.lang.invoke.MethodHandleImpl.BindCaller.T
+
 import spock.lang.IgnoreRest
 import extension.custom.Report
 
@@ -34,7 +37,7 @@ class PartidoSpec extends BaseIntegrationSpec{
 	
 	def 'el ultimo set de un partido debe tener un numero de games mas grande al final que al principio (6-3 y no 3-6)'() {
 		given: 'un partido donde el ganador pierde el ultimo set'
-			def partido = singleODoble
+			def partido = crearSingleFromString(sets)
 		when: 'guardo el partido'
 			partido.save()
 		then: 'no se deberia guardar'
@@ -42,14 +45,13 @@ class PartidoSpec extends BaseIntegrationSpec{
 		and: 'deberia mostrar un mensaje de error acorde'
 			partido.errors.allErrors.toString().contains('El ultimo set debe tener un numero mayor de ' + 
 															'games al principio que final (por ej. 6-3 y no 3-6)')
-		where: 'el partido es un single o un doble'
-			singleODoble << [Single.buildWithoutSave(crearSingle('7-5', '6-4', '3-6')),
-							 Doble.buildWithoutSave(crearDoble('7-5', '6-4', '3-6'))]
+		where: ''
+			sets << ['7-5 6-4 3-6']
 	}
 	
 	def 'un partido debe tener mas sets que ganados por la izquierda que por la derecha (6-3 y no 3-6)'() {
 		given: 'un partido que tiene mas sets ganados por la derecha que por la izquierda'
-			def partido = singleODoble
+			def partido = crearSingleFromString(sets)
 		when: 'guardo el partido'
 			partido.save()
 		then: 'no se deberia guardar'
@@ -57,39 +59,25 @@ class PartidoSpec extends BaseIntegrationSpec{
 		and: 'deberia mostrar un mensaje de error acorde'
 			partido.errors.allErrors.toString().contains('El partido tiene mal los datos de los sets.' +
 																'Los games del ganador deben ir del lado izquierdo de cada set')
-		where: 'el partido es un single o un doble'
-			singleODoble << [Single.buildWithoutSave(crearSingle('7-5', '4-6', '3-6')),
-							 Single.buildWithoutSave(crearSingle('5-7', '4-6', '3-6')),
-							 Single.buildWithoutSave(crearSingle('5-7', '4-6')),
-							 Single.buildWithoutSave(crearSingle('5-7', '4-6', '6-3')),
-							 Single.buildWithoutSave(crearSingle('5-7', '6-4', '3-6')),
-							 Doble.buildWithoutSave(crearDoble('7-5', '4-6', '3-6')),
-							 Doble.buildWithoutSave(crearDoble('5-7', '4-6', '3-6')),
-							 Doble.buildWithoutSave(crearDoble('5-7', '4-6')),
-							 Doble.buildWithoutSave(crearDoble('5-7', '4-6', '6-3')),
-							 Doble.buildWithoutSave(crearDoble('5-7', '6-4', '3-6'))]
+		where: ''
+			    sets << ['7-5 4-6 3-6', '5-7 4-6 3-6', '5-7 4-6', '5-7 4-6 6-3', '4-7 6-4 3-6']
 	}
 	
 	def 'un partido con mas sets ganados por izquierda que por derecha y con el ultimo set ganado por la izquierda se debe poder guardar'() {
 		given: 'un partido con mas sets ganados por izquierda que por derecha y con el ultimo set ganado por la izquierda'
-			def partido = singleODoble
+			def partido = crearSingleFromString(sets)
 		when: 'guardo el partido'
 			partido.save()
 		then: 'se deberia guardar'
 			Partido.findAll().size() > 0
 			Partido.get(partido.id) != null
-		where: 'el partido es un single o un doble'
-			singleODoble << [Single.buildWithoutSave(crearSingle('7-5', '3-6', '6-4')),
-							 Single.buildWithoutSave(crearSingle('5-7', '6-3', '6-4')),
-							 Single.buildWithoutSave(crearSingle('7-6', '6-3')),
-							 Doble.buildWithoutSave(crearDoble('7-5', '3-6', '6-4')),
-							 Doble.buildWithoutSave(crearDoble('5-7', '6-3', '6-4')),
-							 Doble.buildWithoutSave(crearDoble('7-5', '6-3'))]
+		where: ''
+			sets << ['7-5 3-6 6-4', '5-7 6-3 6-4', '7-6 6-3']
 	}
 
 	def 'un partido abandonado no puede tener un resultado normal'() {
 		given: 'un partido abandonado con resultado de un partido normal'
-			def partido = singleODoble
+			def partido = crearSingleAbandonadoFromString(sets)
 		when: 'guardo el partido'
 			partido.save()
 		then: 'no se deberia guardar'
@@ -97,20 +85,13 @@ class PartidoSpec extends BaseIntegrationSpec{
 		and: 'deberia mostrar un mensaje de error acorde'
 			partido.errors.allErrors.toString().contains('El partido esta marcado como abandono, pero ' +
 															'tiene un resultado normal')
-		where: 'el partido es un single o un doble'
-			singleODoble << [Single.buildWithoutSave(crearSingle('7-5', '6-3') + [abandono: true]),
-								Single.buildWithoutSave(crearSingle('7-5', '3-6', '7-6') + [abandono: true]),
-								Single.buildWithoutSave(crearSingle('5-7', '6-3', '6-1') + [abandono: true]),
-								Single.buildWithoutSave(crearSingle('7-5', '6-3') + [abandono: true]),
-								Doble.buildWithoutSave(crearDoble('7-5', '6-3') + [abandono: true]),
-								Doble.buildWithoutSave(crearDoble('7-5', '3-6', '7-6') + [abandono: true]),
-								Doble.buildWithoutSave(crearDoble('5-7', '6-3', '6-1') + [abandono: true]),
-								Doble.buildWithoutSave(crearDoble('7-5', '6-3') + [abandono: true]),]
+		where: ''
+			sets << ['7-5 6-3', '7-5 3-6 7-6', '5-7 6-3 6-1', '7-5 6-3']
 	}
 
 	def 'un partido abandonado no puede tener los 3 sets completos'(){
 		given: 'un partido abandonado con los 3 sets completos'
-			def partido = singleODoble
+			def partido = crearSingleAbandonadoFromString(sets)
 		when: 'guardo el partido'
 			partido.save()
 		then: 'no se deberia guardar'
@@ -118,70 +99,47 @@ class PartidoSpec extends BaseIntegrationSpec{
 		and: 'deberia mostrar un mensaje de error acorde'
 			partido.errors.allErrors.toString().contains('El partido esta marcado como abandono, pero ' +
 															'tiene todos los sets terminados')
-		where: 'el partido es un single o un doble'
-			singleODoble << [Single.buildWithoutSave(crearSingle('7-5', '3-6', '3-6') + [abandono: true]),
-								Doble.buildWithoutSave(crearDoble('7-5', '3-6', '3-6') + [abandono: true]),]
+		where: ''
+			sets << ['7-5 3-6 3-6']
 	}
 
 	def 'un partido abandonado puede tener un resultado anormal'() {
 		given: 'un partido abandonado con resultado anormal'
-			def partido = singleODoble
+			def partido = crearSingleAbandonadoFromString(sets)
 		when: 'guardo el partido'
 			partido.save()
 		then: 'se deberia guardar'
 			Partido.findAll().size() > 0
 			Partido.get(partido.id) != null
-		where: 'el partido es un single o un doble'
-			singleODoble << [Single.buildWithoutSave(crearSingle('7-5', '3-6') + [abandono: true]),
-								Single.buildWithoutSave(crearSingle('7-5') + [abandono: true]),
-								Single.buildWithoutSave(crearSingle('2-1') + [abandono: true]),
-								Doble.buildWithoutSave(crearDoble('7-5', '3-6') + [abandono: true]),
-								Doble.buildWithoutSave(crearDoble('7-5') + [abandono: true]),
-								Doble.buildWithoutSave(crearDoble('2-1') + [abandono: true])]
+		where: ''
+			sets << ['7-5 3-6', '7-5', '2-1']
 	}
-
+	
 	def 'un partido abandonado no puede tener games en un set si el set anterior no fue completado'() {
 		given: 'un partido abandonado con games en el segundo o tercer set, sin tener el primero terminado'
-			def partido = singleODoble
+			def partido = crearSingleAbandonadoFromString(sets)
 		when: 'guardo'
 			partido.save()
 		then: 'no se deberia guardar'
 			Partido.findAll().size() == 0
 		and: 'deberia mostrar un mensaje de error acorde'
 			partido.errors.allErrors.toString().contains('El partido tiene games para un set cuando el set anterior no esta terminado')
-		where: 'el partido es un single o un doble'
-			singleODoble << [Single.buildWithoutSave(crearSingle('2-1', '6-4') + [abandono: true]),
-								Single.buildWithoutSave(crearSingle('2-1', '3-2', '6-7') + [abandono: true]),
-								Single.buildWithoutSave(crearSingle('6-4', '4-5', '7-5') + [abandono: true]),
-								Single.buildWithoutSave(crearSingle('2-1', '2-3') + [abandono: true]),
-							 Doble.buildWithoutSave(crearDoble('2-1', '6-4') + [abandono: true]),
-								Doble.buildWithoutSave(crearDoble('2-1', '3-2', '6-7') + [abandono: true]),
-								Doble.buildWithoutSave(crearDoble('6-4', '4-5', '7-5') + [abandono: true]),
-								Doble.buildWithoutSave(crearDoble('2-1', '2-3') + [abandono: true])]
+		where: ''
+			sets << ['2-1 6-4', '2-1 3-2 6-7', '6-4 4-5 7-5', '2-1 2-3']
 	}
-	
+
 	def 'un partido no puede tener sets sin terminar con games'() {
 		given: 'un partido con sets sin terminar (por ej. 5-4)'
-			def partido = singleODoble
+			def partido = crearSingleFromString(sets)
 		when: 'guardo'
 			partido.save()
 		then: 'no se deberia guardar'
 			Partido.findAll().size() == 0
 		and: 'deberia mostrar un mensaje de error acorde'
 			partido.errors.allErrors.toString().contains('Falta completar el resultado en alguno de los sets')
-		where: 'el partido es un single o un doble'
-			singleODoble << [Single.buildWithoutSave(crearSingle('7-5', '5-3')),
-								Single.buildWithoutSave(crearSingle('7-5', '6-3', '3-4')),
-								Single.buildWithoutSave(crearSingle('2-1', '5-3')),
-								Single.buildWithoutSave(crearSingle('2-1', '6-3', '7-6')),
-								Single.buildWithoutSave(crearSingle('2-1', '3-2', '3-5')),
-								Single.buildWithoutSave(crearSingle('2-1', '3-2', '5-3')),
-								Doble.buildWithoutSave(crearDoble('7-5', '5-3')),
-								Doble.buildWithoutSave(crearDoble('7-5', '6-3', '3-4')),
-								Doble.buildWithoutSave(crearDoble('2-1', '5-3')),
-								Doble.buildWithoutSave(crearDoble('2-1', '6-3', '7-6')),
-								Doble.buildWithoutSave(crearDoble('2-1', '3-2', '3-5')),
-								Doble.buildWithoutSave(crearDoble('2-1', '3-2', '5-3'))]
+		where: ''
+			sets << ['7-5 5-3', '7-5 6-3 3-4', '2-1 5-3',
+					 '2-1 6-3 7-6', '2-1 3-2 3-5', '2-1 3-2 5-3']
 	}
 	
 	private Map crearSingle(primerSet, segundoSet = null, tercerSet = null){
@@ -205,4 +163,17 @@ class PartidoSpec extends BaseIntegrationSpec{
 					.minus([jugadorLocal: jugador1, jugadorVisitante: jugador2])
 	}
 	
+	private Single crearSingleFromString(setsString){
+		def sets = setsString.tokenize(' ')
+		def primerSet = sets[0]
+		def segundoSet = sets.size() >= 2 ?  sets[1] : null
+		def tercerSet = sets.size() >= 3 ? sets[2] : null
+		return Single.buildWithoutSave(crearSingle(primerSet, segundoSet, tercerSet))
+	}
+
+	private Single crearSingleAbandonadoFromString(setsString){
+		def single = crearSingleFromString(setsString)
+		single.abandono = true
+		return single
+	}
 }
